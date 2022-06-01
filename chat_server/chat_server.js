56937df39ad606ba6db090818206093bca28b3c4
DR-Theopolis <100797951+DR-Theopolis@users.noreply.github.com>
2022-06-01 14:33:03 -0700

    Initial commit

---
+++ chat_server/chat_server.js
@@ -0,0 +1,11 @@
+const server = require("socket.io")
+const io = new server.Server(3000)
+io.on("connection", function (socket) {
+    socket.on('new message', function (arg) {
+        console.log(`New message: ${arg}`)
+        socket.broadcast.emit('new message', {
+            //username: socket.username,
+            message: arg
+          });
+    })
+});
