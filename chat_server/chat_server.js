b869b2ea4cbaf9381e26ac6c856e1aa7f2d7b32e
DR-Theopolis <dylanfreespirit@gmail.com>
2022-06-22 13:38:06 -0700

    Fixed repeat bug

--- chat_server/chat_server.js
+++ chat_server/chat_server.js
@@ -3,7 +3,7 @@ const io = new server.Server(3000)
 io.on("connection", function (socket) {
     socket.on('new message', function (arg) {
         console.log(`New message: ${arg}`)
-        io.emit('new message', {
+        socket.broadcast.emit('new message', {
             //username: socket.username,
             message: arg
           });
