c8ceae01ed1deaba96a3e3f46f4c67c8980a2f27
Matt Wareham <slog@slogmaverick.com>
2022-07-07 05:18:35 -1000

    updated notes for new features.

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
-# chat
+# Chat Future Features
+- Write messages to a file on the server.
+- Send old messages to new user login.
+- Video messaging.
+- Username from prompt on first login.
+- Save username on client-side local storage.
+- Cache all usernames on server side.  
+- Attach images to a message.
+- Have different rooms.  
  
 
- ## GIT
- git add <filename> \
- git commit -m "comments go here" \
- git push -u origin main
+## GIT
+- git pull 
+- git add <filename> or git add .
+- git commit -m "comments go here" 
+- git push -u origin main
--- chat_server/chat_server.js
+++ chat_server/chat_server.js
@@ -1,8 +1,24 @@
 const server = require("socket.io")
 const io = new server.Server(3000)
+
+
+
 io.on("connection", function (socket) {
+    //Read old messages from file.
+    //Send all old messages to this new user.
+
+
     socket.on('new message', function (arg) {
         console.log(`New message: ${arg}`)
+
+        let newMsg = {
+            timestamp: Date.now(),
+            userName: socket.username,
+            msg: arg
+        };
+
+        //Append to file.
+
         socket.broadcast.emit('new message', {
             //username: socket.username,
             message: arg
