acdfe15a3135c0ba995a28ad345faf6b0a76d85a
DR-Theopolis <dylanfreespirit@gmail.com>
2022-08-11 08:13:19 -0700

    updates

--- chat_server/chat_server.js
+++ chat_server/chat_server.js
@@ -1,27 +1,19 @@
+const fs = require('fs')
 const server = require("socket.io")
 const io = new server.Server(3000)
-
-
-
 io.on("connection", function (socket) {
-    //Read old messages from file.
-    //Send all old messages to this new user.
-
-
+    socket.on('new user', function() {
+        const file = fs.readFileSync('/Users/error/Desktop/git-chat/messages.txt',
+        {encoding:'utf8', flag:'r'})
+        console.log(file);
+        socket.emit('new message', {
+            message: file
+          });
     socket.on('new message', function (arg) {
+        fs.appendFileSync("messages.txt", `${arg}<br>\r\n`);
         console.log(`New message: ${arg}`)
-
-        let newMsg = {
-            timestamp: Date.now(),
-            userName: socket.username,
-            msg: arg
-        };
-
-        //Append to file.
-
         socket.broadcast.emit('new message', {
-            //username: socket.username,
             message: arg
           });
     })
-});
+})});
